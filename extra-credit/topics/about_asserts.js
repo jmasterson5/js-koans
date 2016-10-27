fa85d2d24d1ffd8bbcbaf3a9e75a6d0f75dedc69
Antoine Stimphil <antoinedstimphil@gmail.com>
2016-10-26 22:32:30 -0400

    Update about_asserts.js
    Extra Credit - AntoineDS

--- extra-credit/topics/about_asserts.js
+++ extra-credit/topics/about_asserts.js
@@ -14,8 +14,8 @@ test("equal", function() {
     equal(__, 1 + 1, 'what will satisfy the equal assertion?');
 });
 
-The About Asserts Function work as so, === is a strict equality operator meaning that other end of the equation can only be stricly
+/* The About Asserts Function work as so, === is a strict equality operator meaning that other end of the equation can only be stricly
 equaly to the other part. For Exmaple if _____ === Hotdog, that means other part can stricly only be Hotdog, and no other answer will
 provide a solution. The same goes for the __, 1 + 1 in math 1+1 = 2. The only possible answer that will satisfy what 1+1 would be 2. The
-equation is using common math to figure an out equation and is asking what would 1+1 = ?.
+equation is using common math to figure an out equation and is asking what would 1+1 = ?. */
 
