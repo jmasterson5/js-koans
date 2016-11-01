741726b11832d6d67d09be245473abd8ccb85308
James Pryde <ClicheUsername@users.noreply.github.com>
2016-11-01 12:37:20 -0400

    Explained "Dynamically Adding Properties" test

--- extra-credit/topics/about_objects.js
+++ extra-credit/topics/about_objects.js
@@ -15,6 +15,16 @@ test("object literal notation", function() {
     equal(102, person.age, "what is the person's age?");
 });
 
+//The Dynamically Adding Properties test works by comparing the value (Either string, or int in this case)
+//to the property of the person object which correlates. On the first line in this function, an empty object
+//named person is created. So this object exists, but contains no data yet. On the second and third lines 
+//if you replace the __ with name and age respectively, you are then adding and setting these properties in
+//the person object. So by the time the "equal" lines are run, the person object now contains the property
+//"Name" which is set to "Amory Blaine", and the property "age" which is set to 102.
+//This means that when the fourth line runs, it is going to compare the string "Amory Blaine" with
+//whatever person.name's value is (In this case it's value is also "Amory Blaine"), which means the test will
+//pass! The same exact concept applies to the final line, comparing the integer 102 to the property person.age
+//which also equals 102.
 test("dynamically adding properties", function() {
     var person = {};
     person.__ = "Amory Blaine";
