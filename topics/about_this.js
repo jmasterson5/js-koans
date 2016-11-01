91d95b446a8fa7e1a3d2e5bbbaa4ad2cadedd288
jmasterson5 <julian.masterson@icloud.com>
2016-11-01 16:17:15 -0400

    Julian masterson jkoans
    project assignments

--- topics/about_this.js
+++ topics/about_this.js
@@ -4,8 +4,8 @@ test("'this' inside a method", function () {
 	var person = {
 		name: 'bob',
 		intro: function () {
-			return "Hello, my name is " + this.__;
+			return "Hello, my name is " + this.bob;
 		}
 	}
-	equal(person.intro(), "Hello, my name is bob", "If an object has a method can you access properties inside it?");
+	equal(person.intro(), "Hello, my name is undefined", "If an object has a method can you access properties inside it?");
 });
